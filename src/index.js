89b032d6d27c32139b62b9639e140ded04e793b0
markwade <94111255+markwade73@users.noreply.github.com>
2022-05-31 23:57:05 -0300

    saving-progress

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>hi world</h1>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
